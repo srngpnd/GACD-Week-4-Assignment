b27041a8403d66ff7ab06c413ce3d9709bdeb9ed
Sarang Pande <pande.srng@gmail.com>
2019-01-18 15:13:09 +0530

    Week 4 assignment .1

---
+++ run_analysis.R
@@ -0,0 +1,45 @@
+# import dplyr library
+library(dplyr)
+
+# read data from train folder
+X_train <- read.table("../UCI HAR Dataset/train/X_train.txt")
+Y_train <- read.table("../UCI HAR Dataset/train/Y_train.txt")
+Sub_train <- read.table("../UCI HAR Dataset/train/subject_train.txt")
+
+# read data from test folder
+X_test <- read.table("../UCI HAR Dataset/test/X_test.txt")
+Y_test <- read.table("../UCI HAR Dataset/test/Y_test.txt")
+Sub_test <- read.table("../UCI HAR Dataset/test/subject_test.txt")
+
+# read data from features
+features <- read.table("../UCI HAR Dataset/features.txt")
+
+# read data from activity labels
+activity_labels <- read.table("../UCI HAR Dataset/activity_labels.txt")
+
+# Objectives:
+
+# 1. Merges the training and the test sets to create one data set.
+X_total <- rbind(X_train, X_test)
+Y_total <- rbind(Y_train, Y_test)
+Sub_total <- rbind(Sub_train, Sub_test)
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+selected_var <- features[grep("mean\\(\\)|std\\(\\)",features[,2]),]
+X_total <- X_total[,selected_var[,1]]
+
+# 3. Uses descriptive activity names to name the activities in the data set
+colnames(Y_total) <- "activity"
+Y_total$activitylabel <- factor(Y_total$activity, labels = as.character(activity_labels[,2]))
+activitylabel <- Y_total[,-1]
+
+# 4. Appropriately labels the data set with descriptive variable names.
+colnames(X_total) <- features[selected_var[,1],2]
+
+# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+colnames(Sub_total) <- "subject"
+total <- cbind(X_total, activitylabel, Sub_total)
+total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
+
+# Finally write the result into a text file
+write.table(total_mean, file = "../UCI HAR Dataset/final_result_tidy_data.txt", row.names = FALSE, col.names = TRUE)
